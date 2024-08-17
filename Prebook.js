eafaed311a23e827e6b9bf9e0c4dc7d771c7ec9a
ankanadatta <adatta751@gmail.com>
2024-08-17 23:39:59 +0530

    Add files via upload

---
+++ Prebook.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Prebook = () => {
+  let headStyle = {
+    color: "#F2F2F2",
+    "margin-top": "70px",
+    "margin-bottom": "30px",
+    "text-shadow": "5px 1px 5px #131313"
+  }
+  return (
+    <div className='container'>
+      <h1 style={headStyle}>Pre-book Now</h1>
+    </div>
+  )
+}
+
+export default Prebook
